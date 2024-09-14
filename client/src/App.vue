b91eb9105149ad4c4e228e5b92b2d02eef39210c
Heesnag Whang <hwhang0917@gmail.com>
2024-09-15 01:14:53 +0900

    Setup basic frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,121 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const x = ref(0);
+import { ref, computed, watch } from 'vue'
+import { useWebSocket, useIntervalFn } from '@vueuse/core'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Progress } from '@/components/ui/progress'
+import { Skeleton } from '@/components/ui/skeleton'
+
+const { data, send } = useWebSocket('ws://localhost:8080/ws/otp')
+
+const textInput = ref()
+const askText = ref()
+const isDialogOpen = ref(false)
+useIntervalFn(() => {
+  if (!askText.value) return
+  send(
+    JSON.stringify({
+      ask_text: askText.value
+    })
+  )
+}, 1000)
+const resetAskText = () => {
+  askText.value = ''
+  textInput.value = ''
+}
+const setAskText = () => {
+  askText.value = textInput.value
+  isDialogOpen.value = true
+}
+const parsedData = computed(() => JSON.parse(data.value))
+const progress = computed(() => {
+  if (parsedData.value && parsedData.value.success) {
+    return (parsedData.value.time_left / 30) * 100
+  }
+  return 0
+})
+const isLoading = computed(() => !parsedData.value)
+
+watch(
+  () => isDialogOpen.value,
+  (value) => {
+    if (!value) {
+      resetAskText()
+    }
+  }
+)
 </script>
 
 <template>
-    <div class="flex w-screen justify-center text-red-500">
-        <p>{{ x }}</p>
-    </div>
+  <main class="flex h-screen w-screen items-center justify-center">
+    <Card class="w-2/3 p-4">
+      <CardHeader>
+        <CardTitle>Countersign</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <div class="mb-2 flex gap-1">
+            <p>인증받을 문어를 입력해주세요.</p>
+            <HoverCard>
+              <HoverCardTrigger class="cursor-pointer underline">(?)</HoverCardTrigger>
+              <HoverCardContent class="w-120">
+                <p class="font-bold">문어는 구두로 말하기 쉬운 단어일 수록 좋습니다.</p>
+                <small>예) 바나나, 바람, 종달새</small>
+              </HoverCardContent>
+            </HoverCard>
+          </div>
+          <Input v-model="textInput" @keyup.enter="setAskText" />
+        </CardDescription>
+      </CardContent>
+      <CardFooter class="flex justify-center px-6 pb-6">
+        <Button class="w-full" @click="setAskText" :disabled="!textInput">인증 단어 조회</Button>
+      </CardFooter>
+    </Card>
+    <Dialog v-model:open="isDialogOpen">
+      <DialogContent>
+        <DialogHeader v-if="isLoading">
+          <DialogTitle class="mb-4">
+            <Skeleton class="h-7 w-40 bg-gray-200" />
+          </DialogTitle>
+          <DialogDescription>
+            <Skeleton class="h-5 w-full bg-gray-200" />
+          </DialogDescription>
+          <DialogDescription>
+            <Skeleton class="h-5 w-full bg-gray-200" />
+          </DialogDescription>
+        </DialogHeader>
+        <DialogHeader v-else>
+          <DialogTitle class="mb-4">문어: {{ askText }} / 답어: {{ parsedData.otp }}</DialogTitle>
+          <DialogDescription>
+            상대방에게 문어를 말해주세요. 그리고 제한시간 내에 답어를 확인해주세요.
+          </DialogDescription>
+          <Progress :model-value="progress" />
+          <DialogDescription> {{ parsedData.time_left }}초 동안 유효합니다. </DialogDescription>
+        </DialogHeader>
+        <DialogFooter v-if="isLoading">
+          <Skeleton class="h-10 w-20 bg-gray-200" />
+        </DialogFooter>
+        <DialogFooter v-else>
+          <Button @click="isDialogOpen = false">초기화</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </main>
 </template>
--- server/internal/websocket/websocket.go
+++ server/internal/websocket/websocket.go
@@ -12,13 +12,13 @@ import (
 )
 
 type Request struct {
-	Command string `json:"command"`
 	AskText string `json:"ask_text"`
 }
 type Response struct {
-	Command string `json:"command"`
-	Success bool   `json:"success"`
-	Data    string `json:"data"`
+	Success  bool   `json:"success"`
+	AskText  string `json:"ask_text,omitempty"`
+	OTP      string `json:"otp,omitempty"`
+	TimeLeft string `json:"time_left,omitempty"`
 }
 
 func UpgradeWebSocket(c *fiber.Ctx) error {
@@ -34,7 +34,6 @@ func GetOTPHandler() fiber.Handler {
 		ip := c.IP()
 		log.Printf("Client connected [%s]", ip)
 
-		askKey := c.Params("askKey")
 		var req Request
 		var res Response
 
@@ -52,26 +51,18 @@ func GetOTPHandler() fiber.Handler {
 				continue
 			}
 
-			switch req.Command {
-			case "ask":
-				otp := hash.GetOTP(askKey)
-				res.Success = true
-				res.Command = "ask"
-				res.Data = otp
-				break
-			case "time":
-				interval := config.GetInterval()
-				timeLeft := strconv.FormatInt(hash.GetTimeLeft(int64(interval)), 10)
-				res.Success = true
-				res.Command = "time"
-				res.Data = timeLeft
-				break
-			default:
+			if req.AskText == "" {
 				res.Success = false
-				res.Command = req.Command
-				res.Data = "Invalid command"
-				break
+				res.AskText = ""
+				res.OTP = ""
+				res.TimeLeft = ""
+			} else {
+				res.Success = true
+				res.AskText = req.AskText
+				res.OTP = hash.GetOTP(req.AskText)
+				res.TimeLeft = strconv.FormatInt(hash.GetTimeLeft(int64(config.GetInterval())), 10)
 			}
+
 			if err := c.WriteJSON(res); err != nil {
 				log.Fatalf("Error writing message: %v", err)
 			}
